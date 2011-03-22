3583176093a47cf68e189da830eab2e7ee947761
wschurman <wschurman@gmail.com>
2011-03-22 19:41:01 -0400

    modified

--- Controller.php
+++ Controller.php
@@ -7,10 +7,22 @@ class Controller {
 		$this->responseXml = simplexml_load_file("http://www.recipepuppy.com/api/?i=$ingredient&q=$query&p=1&format=xml");
 	}
 	
+	
 	public function getIngredientData($ingredient) {
+		//cache/memoize it
 		$data = array();
 		
+		$headers = array(
+			'oauth_consumer_key' => '',
+			'oauth_nonce' => '',
+			'oauth_signature_method' => 'HMAC-SHA1',
+			'oauth_timestamp' => '',
+			'oauth_version' => '1.0',
+			
+		);
+		$normalized = "POST&http%3A%2F%2Fplatform.fatsecret.com%2Frest%2Fserver.api&"
 		
+		$ing_xml = simplexml_load_file("http://platform.fatsecret.com/rest/server.api?food_id=33691&method=food.get&oauth_consumer_key=9a1a6fd1-fff5-433f-9dd7-7daa4587bf5d&oauth_nonce=1234&oauth_signature=sAyYTJiIxOGkvFpBcH8L%2BlFQRCQ%3D&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1245126631&oauth_version=1.0");
 	}
 	
 	public function processData() {
