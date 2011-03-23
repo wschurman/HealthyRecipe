4cacd616de53dee1cf3c8f2d19cf39cf406be47c
wschurman <wschurman@gmail.com>
2011-03-22 22:43:39 -0400

    lala

--- Controller.php
+++ Controller.php
@@ -1,5 +1,8 @@
 <?php
 
+require_once('lib/FatSecretAPI.php');
+require_once('lib/config.php');
+
 class Controller {
 	public $responseXml;
 	
@@ -12,17 +15,8 @@ class Controller {
 		//cache/memoize it
 		$data = array();
 		
-		$headers = array(
-			'oauth_consumer_key' => '',
-			'oauth_nonce' => '',
-			'oauth_signature_method' => 'HMAC-SHA1',
-			'oauth_timestamp' => '',
-			'oauth_version' => '1.0',
-			
-		);
-		$normalized = "POST&http%3A%2F%2Fplatform.fatsecret.com%2Frest%2Fserver.api&"
+		$API = new FatSecretAPI(API_KEY, API_SECRET);
 		
-		$ing_xml = simplexml_load_file("http://platform.fatsecret.com/rest/server.api?food_id=33691&method=food.get&oauth_consumer_key=9a1a6fd1-fff5-433f-9dd7-7daa4587bf5d&oauth_nonce=1234&oauth_signature=sAyYTJiIxOGkvFpBcH8L%2BlFQRCQ%3D&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1245126631&oauth_version=1.0");
 	}
 	
 	public function processData() {
---
+++ lib/config.php
@@ -0,0 +1,11 @@
+<?php
+
+define('API_URL', 'http://platform.fatsecret.com/rest/server.api');
+
+//please register at http://platform.fatsecret.com for an API KEY 
+define('API_KEY', 'f2738ae1925a421b878a15b49395c6ea');
+
+//please register at http://platform.fatsecret.com for an API SECRET
+define('API_SECRET', '1d4aa7b387f34f2b906b1fca7f3a38a1'); 
+
+?>
